afbb977700861fcc2232837480340db96bf0817e
Horn Narin <hornnarin9@gmail.com>
2026-06-30 07:20:11 +0700

    first commit

---
+++ src/main/java/com/example/reactivestreamfundamentals/ReactiveStreamFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactivestreamfundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveStreamFundamentalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveStreamFundamentalsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reactivestreamfundamentals/ReactiveStreamFundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactivestreamfundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveStreamFundamentalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
